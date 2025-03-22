a6d2b680903819a1c8f440f188ed60a71acea1c7
jr <junior_silvapereira@hotmail.com>
2025-03-22 00:00:50 -0300

    fix: auth.guard.ts

--- src/modules/auth/auth.guard.ts
+++ src/modules/auth/auth.guard.ts
@@ -4,6 +4,7 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { FastifyRequest } from 'fastify';
 
 import { AuthService } from './auth.service';
 
@@ -12,7 +13,7 @@ export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService) { }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
+    const request = context.switchToHttp().getRequest<FastifyRequest>();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
       throw new UnauthorizedException();
@@ -25,8 +26,19 @@ export class AuthGuard implements CanActivate {
     return true;
   }
 
-  private extractTokenFromHeader(request): string | undefined {
-    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+  private extractTokenFromHeader(request: FastifyRequest): string | undefined {
+    const authorizationHeader = request.headers.authorization;
+
+    if (typeof authorizationHeader !== 'string') {
+      return undefined;
+    }
+
+    const [type, token] = authorizationHeader.split(' ');
+
+    if (type !== 'Bearer' || typeof token !== 'string') {
+      return undefined;
+    }
+
     return type === 'Bearer' ? token : undefined;
   }
 }
