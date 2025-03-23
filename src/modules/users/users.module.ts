d386151004b3c7401b010775c7d60b4c4fe2c4fe
jr <junior_silvapereira@hotmail.com>
2025-03-23 10:46:19 -0300

    feat: users module forwardref

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,14 +1,16 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { OrdersModule } from '../orders/orders.module';
 import { User } from './entities/user.entity';
 import { BcryptjsHashProvider } from './hash-provider/bcrypt-hash.provider';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => OrdersModule)],
   controllers: [UsersController],
   providers: [UsersService, BcryptjsHashProvider],
+  exports: [UsersService],
 })
-export class UsersModule { }
+export class UsersModule {}
