e8952b28aa73df22e607c32179f565ecac4d3547
jr <junior_silvapereira@hotmail.com>
2025-03-23 10:47:28 -0300

    feat: auth - service and module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,15 +1,19 @@
 import { Global, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 import { EnvConfigModule } from '@/shared/infra/env-config/env-config.module';
 import { EnvConfigService } from '@/shared/infra/env-config/env-config.service';
 
+import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Global()
 @Module({
   imports: [
     EnvConfigModule,
+    UsersModule,
     JwtModule.registerAsync({
       imports: [EnvConfigModule],
       useFactory: (configService: EnvConfigService) => ({
@@ -19,8 +23,9 @@ import { AuthService } from './auth.service';
       }),
       inject: [EnvConfigService],
     }),
+    PassportModule,
   ],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
   exports: [AuthService],
 })
-export class AuthModule { }
+export class AuthModule {}
