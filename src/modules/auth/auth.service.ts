9a6f5ae84de5d754e3396dc0f973729c9d687915
jr <junior_silvapereira@hotmail.com>
2025-03-23 10:47:40 -0300

    feat: auth - service and module

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { compare } from 'bcrypt';
 
 import { EnvConfigService } from '../../shared/infra/env-config/env-config.service';
+import { UsersService } from '../users/users.service';
 
 type GenerateJwtProps = {
   accessToken: string;
@@ -12,7 +14,18 @@ export class AuthService {
   constructor(
     private jwtService: JwtService,
     private configService: EnvConfigService,
+    private usersService: UsersService,
   ) {}
+
+  async validateUser(email: string, password: string): Promise<{ id: string }> {
+    const user = await this.usersService.findByEmail(email);
+    if (!user) throw new UnauthorizedException('User not found!');
+    const isPasswordMatch = await compare(password, user.password);
+    if (!isPasswordMatch)
+      throw new UnauthorizedException('Invalid credentials');
+
+    return { id: user.id };
+  }
 
   async generateJwt(userId: string): Promise<GenerateJwtProps> {
     const accessToken = await this.jwtService.signAsync({ id: userId }, {});
