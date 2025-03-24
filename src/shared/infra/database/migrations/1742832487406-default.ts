16d4cf80d442de90a415197fd5a83e5ab2b360eb
jr <junior_silvapereira@hotmail.com>
2025-03-24 13:12:33 -0300

    feat: change postgres version ci.yml

--- src/shared/infra/database/migrations/1742832487406-default.ts
+++ src/shared/infra/database/migrations/1742832487406-default.ts
@@ -1,16 +1,14 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Default1742832054101 implements MigrationInterface {
-    name = 'Default1742832054101'
+export class Default1742832487406 implements MigrationInterface {
+    name = 'Default1742832487406'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TYPE "public"."user_role_enum" AS ENUM('ADMIN', 'EDITOR', 'USER')`);
         await queryRunner.query(`ALTER TABLE "user" ADD "role" "public"."user_role_enum" NOT NULL DEFAULT 'USER'`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "role"`);
-        await queryRunner.query(`DROP TYPE "public"."user_role_enum"`);
     }
 
 }
