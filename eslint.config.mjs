76f83fe92ffddc5633c574aa94a802c007a797ed
jr <junior_silvapereira@hotmail.com>
2025-04-30 21:16:57 -0300

    fix eslint prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,17 @@
 // @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
-import simpleImportSort from 'eslint-plugin-simple-import-sort';
-import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import eslint from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ["eslint.config.mjs"],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-
+  eslintPluginPrettierRecommended,
   {
     languageOptions: {
       globals: {
@@ -20,7 +19,7 @@ export default tseslint.config(
         ...globals.jest,
       },
       ecmaVersion: 5,
-      sourceType: 'module',
+      sourceType: "module",
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
@@ -29,17 +28,27 @@ export default tseslint.config(
   },
   {
     plugins: {
-      'simple-import-sort': simpleImportSort,
+      "simple-import-sort": simpleImportSort,
     },
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
-      'simple-import-sort/imports': 'error',
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-floating-promises": "warn",
+      "@typescript-eslint/no-unsafe-argument": "warn",
+      "simple-import-sort/imports": "error",
+      "prettier/prettier": [
+        "error",
+        {
+          printWidth: 80,
+          tabWidth: 2,
+          singleQuote: true,
+          trailingComma: "all",
+          arrowParens: "always",
+          semi: true,
+          bracketSpacing: true,
+        },
+      ],
     },
   },
-  eslintPluginPrettierRecommended,
-  eslintConfigPrettier,
 );
